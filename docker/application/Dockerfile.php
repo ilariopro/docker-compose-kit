ac24e34eb5ee0335bda9c971bedb3294657cf13e
il-ario <promutico.ilario@gmail.com>
2025-11-09 19:18:11 +0100

    fix: install composer in php dockerfile

--- docker/application/Dockerfile.php
+++ docker/application/Dockerfile.php
@@ -16,6 +16,8 @@ RUN apt-get update && apt-get install -y \
     && docker-php-ext-enable redis \
     && apt-get clean && rm -rf /var/lib/apt/lists/*
 
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
 WORKDIR /usr/src/app
 
 CMD ["php-fpm", "-F"]
